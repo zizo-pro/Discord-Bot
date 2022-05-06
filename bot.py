4e2802c68bad6919b78b34d0c510cb30e24ba345
ZIZO <ziadezz12@gmail.com>
2022-05-06 22:08:09 +0200

    made the database and added bad words command

--- bot.py
+++ bot.py
@@ -4,23 +4,30 @@ from pickle import TRUE
 from tokenize import Ignore
 from click import pass_obj
 import discord
-from discord.ext import commands, join
+from discord.ext import commands
 from keep_alive import keep_alive
 from discord_components import DiscordComponents, Button, ButtonStyle
 import sqlite3
 
 db = sqlite3.connect("bot_database.db")
 cr = db.cursor()
-EmojiLink = "https://cdn.discordapp.com/emojis/733293111228366871.gif"
+EmojiLink = "https://www.youtube.com/watch?v=iik25wqIuFo"
 
 
 def read_token():
 		with open("token.txt","r") as f:
 				token = f.readline()
 				return token
-
-client = commands.Bot(command_prefix = "!" )
-Blocked_Words = ["1st", "2nd", "3rd"]
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix = "!" ,intents=intents)
+def get_bad_words():
+	global Blocked_Words
+	cr.execute("SELECT bad_word FROM bad_words")
+	data = cr.fetchall()
+	Blocked_Words = []
+	for i in range(len(data)):
+		Blocked_Words.append(data[i][0])
+get_bad_words()
 
 @client.event
 async def on_ready():
@@ -51,35 +58,38 @@ async def button(ctx):
 #Delete Bad Words#
 @client.listen('on_message')
 async def BadWords(message):
-	if message.content in Blocked_Words :
-		await message.channel.purge(limit=1)
-		await message.channel.send("Bad Word :shushing_face:")
-
-
-# intents = discord.Intents.default()
-# intents.members = True
-# client = commands.Bot(command_prefix=',', intents=intents)
-
-# @client.event 
-# async def on_member_join(member):
-# 	emb=discord.Embed(title="NEW MEMBER",description=f"Thanks {member.name} for joining!")
-#     await channel.send(embed=emb)
-
-
-@client.command
-async def join(context):
-	channel = context.author.voice.channel
-	await channel.connect()
+	for txt in Blocked_Words:
+		if "Admin" not in str(message.author.roles) and txt in str(message.content.lower()) and "BOT" not in str(message.author.roles):
+			await message.channel.purge(limit=1)
 
 
 
+@client.event
+async def on_member_join(member):
+	print("member joined")
+	emb=discord.Embed(title="NEW MEMBER",description=f"Thanks {member.name} for joining!")
+	await client.get_channel(839671710856773632).send(embed=emb)
 
+# @client.command
+# async def join(ctx):
+# 	channel = ctx.author.voice.channel
+# 	await channel.connect()
 
-# @client.event
-# async def on_message(msg):
-# 		for text in Blocked_Words:
-# 				if "Admin" not in str(msg.author.roles) and text in str(msg.content.lower()):
-# 					await msg.delete()
+@client.command()
+async def mention(message):
+	await message.channel.send(f"{message.author.mention} done")
+
+@client.command()
+async def add_bad_word(message):
+	ind = str(message.message.content.find(" "))
+	msg = message.message.content[int(ind)+1:]
+	if msg in Blocked_Words:
+		await message.channel.send(f"Already Exists")
+	else:
+		cr.execute(f"INSERT INTO bad_words (bad_word) VALUES ('{msg}')")
+		db.commit()
+		get_bad_words()
+		await message.channel.send(f"Word added ||{msg}||")
 
 
 
