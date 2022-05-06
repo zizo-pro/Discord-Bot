7c2b060e5fbf985ca6dc40eafb47d72c2ef078ed
wally <wallyallahninja@gmail.com>
2022-05-06 18:39:42 +0200

    Some Progress

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 from ast import alias
+from codecs import ignore_errors
+from pickle import TRUE
+from tokenize import Ignore
+from click import pass_obj
 import discord
 from discord.ext import commands
 from keep_alive import keep_alive
@@ -9,19 +13,20 @@ db = sqlite3.connect("bot_database.db")
 cr = db.cursor()
 EmojiLink = "https://cdn.discordapp.com/emojis/733293111228366871.gif"
 
+
 def read_token():
 		with open("token.txt","r") as f:
 				token = f.readline()
 				return token
 
 client = commands.Bot(command_prefix = "!" )
+Blocked_Words = ["1st", "2nd", "3rd"]
 
 @client.event
 async def on_ready():
 		print(f"Bot logged in as {client.user}")
 		DiscordComponents(client)
 
-Blocked_Words = ["1st", "2nd", "3rd"]
 @client.command()
 async def ping(ctx):
 	await ctx.send('pong!')
@@ -30,26 +35,48 @@ async def ping(ctx):
 async def button(ctx):
 	await ctx.send(
 			"this is a button test :smile:"
-			,components=[
-				Button(style=ButtonStyle.blue, label="Button 1"),
-				Button(style=ButtonStyle.red, label="Button 2"),
-				Button(style=ButtonStyle.URL, label="This is a Picture!", url=EmojiLink)])
+			,components=[[
+				Button(style=ButtonStyle.blue,
+				label="Button 1"),
+				Button(style=ButtonStyle.red,
+				label="Button 2"),
+				Button(style=ButtonStyle.URL,
+				label="This is a Picture!",
+				url=EmojiLink)]])
 
 	res = await client.wait_for("button_click")
 	if res.channel == ctx.channel :
-		await res.respond(
-			type=4,content= "test"
-		)
+		await res.message.channel.send(EmojiLink)
+
+#Delete Bad Words#
+@client.listen('on_message')
+async def BadWords(message):
+	if message.content in Blocked_Words :
+		await message.channel.purge(limit=1)
+		await message.channel.send("Bad Word :shushing_face:")
 
 @client.event
-async def on_message(msg):
-		for text in Blocked_Words:
-				if "Admin" not in str(msg.author.roles) and text in str(msg.content.lower()):
-					await msg.delete()
-				else:
-					@client.command()
-					async def ping(ctx):
-						await ctx.send('pong!')
+async def on_member_join(member):
+	guild = member.guild
+	if guild.system_channel is not None:
+		to_send = f'Welcome {member.mention} to {guild.name}!'
+		await guild.system_channel.send(to_send)
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = MyClient(intents=intents)
+
+
+
+
+
+
+# @client.event
+# async def on_message(msg):
+# 		for text in Blocked_Words:
+# 				if "Admin" not in str(msg.author.roles) and text in str(msg.content.lower()):
+# 					await msg.delete()
 
 
 
